be5a7fabb56feb93a6173a18cb304d97acf60baa
ArtiSnt <ag1545959@gmail.com>
2025-10-09 11:35:22 +0300

    Added async await task

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+Stopwatch stopwatch = new Stopwatch();
+
+stopwatch.Start();
+Console.WriteLine(ProccesData("Файл1"));
+Console.WriteLine(ProccesData("Файл2"));
+Console.WriteLine(ProccesData("Файл3"));
+
+stopwatch.Stop();
+Console.WriteLine(stopwatch.Elapsed);
+stopwatch.Reset();
+
+List<Task> tasks =
+[
+    ProccesDataAsync("File1"),
+    ProccesDataAsync("File2"),
+    ProccesDataAsync("File3")
+];
+
+stopwatch.Start();
+await Task.WhenAll(tasks);
+stopwatch.Stop();
+Console.WriteLine(stopwatch.Elapsed);
+
+string ProccesData(string dataName)
+{
+    Thread.Sleep(3000);
+    return $"Обработка '{dataName}' завершена за 3 секунды";
+}
+
+async Task ProccesDataAsync(string dataName)
+{
+    await Task.Delay(3000);
+    Console.WriteLine($"Обработка '{dataName}' завершена за 3 секунды");
+}
